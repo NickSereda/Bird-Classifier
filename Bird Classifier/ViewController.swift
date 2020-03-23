830762fb1afaec3f1aa556e251973c29b70f3d1d
Mykola Sereda <acekol1acekol@gmail.com>
2020-03-23 22:10:33 +0200

    UI Build, UI elements connect

--- Bird Classifier/ViewController.swift
+++ Bird Classifier/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+
+    @IBOutlet weak var birdsTableView: UIImageView!
+
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func cameraButtonClicked(_ sender: Any) {
+    
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        cell.descriptionLabel.text = "This is a bird"
+        return cell
+    }
+    
+
+    
 
 }
 

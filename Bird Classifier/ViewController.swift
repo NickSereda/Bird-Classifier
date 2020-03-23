05a06c9829b11195061e71de4262f86404dd1cf4
Mykola Sereda <acekol1acekol@gmail.com>
2020-03-23 22:49:00 +0200

    CoreML model add

--- Bird Classifier/ViewController.swift
+++ Bird Classifier/ViewController.swift
@@ -7,23 +7,85 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
-
+    @IBOutlet weak var birdImageView: UIImageView!
+    
     @IBOutlet weak var birdsTableView: UIImageView!
 
-    
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.isHidden = true
     }
 
     @IBAction func cameraButtonClicked(_ sender: Any) {
-    
+    let picker = UIImagePickerController()
+    picker.delegate = self
+    picker.sourceType = .photoLibrary
+    self.present(picker, animated: true, completion: nil)
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+         if let chosenImage = info[.originalImage] as? UIImage {
+               
+               birdImageView.image = chosenImage
+                   
+                   guard let ciimage = CIImage(image: chosenImage) else {
+                       print("error")
+                       return
+                   }
+               detect(image: ciimage)
+               
+               self.dismiss(animated: true, completion: nil)
+           }
+    }
+    
+     //MARK: - ML Method
+    
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: BirdImageClassifier_1().model) else {
+            fatalError("Loading CoreML Model Failed")
+        }
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+      //  print(request.results)
+            if let firstResult = results.first {
+                var title = firstResult.identifier.uppercased()
+                self.navigationItem.title = title
+                self.tableView.isHidden = false
+//                var titleString = firstResult.identifier.lowercased()
+//                var a = ""
+//                var b = ""
+//                if let first = titleString.components(separatedBy: " ").first {
+//                    a = first
+//                }
+//                if let last = titleString.components(separatedBy: " ").last {
+//                    b = last
+//                }
+//
+              //  self.wikiManager.fetchDataFromWiki(flower: "\(a)%20\(b)")
+                    
+                } else {
+                    self.navigationItem.title = "Unknown bird"
+                }
+        }
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            try handler.perform([request])
+        }
+        catch {
+            print("error")
+        }
+    }
+    
+    //MARK: - Table View Methods
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
@@ -36,7 +98,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
 
-    
 
 }
 

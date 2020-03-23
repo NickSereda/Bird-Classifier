5de0c1c976a6c386a1afb0a2734d517768b32a1a
Mykola Sereda <acekol1acekol@gmail.com>
2020-03-23 14:53:28 +0200

    Initial Commit

---
+++ Bird Classifier/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bird Classifier
+//
+//  Created by Mykola Sereda on 23.03.2020.
+//  Copyright © 2020 Mykola Sereda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
